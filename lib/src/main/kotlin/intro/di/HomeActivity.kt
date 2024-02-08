60b0f5fb0d2ae0f04f4c3a40dcf43e9328273434
Eric Liu <eric.liu@uber.com>
2024-02-08 10:24:47 -0800

    removes impls

--- lib/src/main/kotlin/intro/di/HomeActivity.kt
+++ lib/src/main/kotlin/intro/di/HomeActivity.kt
@@ -1,28 +1,19 @@
 package intro.di
 
-import motif.ScopeFactory
-
 class HomeActivity {
-    private var profile: Profile? = null
-    lateinit var rootScope: RootScope
 
     fun onCreate() {
-        rootScope = ScopeFactory.create(RootScope::class.java, object : RootScope.Dependencies{} )
     }
 
     /**
      * Login the user.
      *//**/
     fun login() {
-        profile = rootScope.authService().login("el@uber.com", "xxxx")
     }
 
     /**
      * Request a ride.
      */
     fun requestRide() {
-        profile?.let {
-            rootScope.loggedInScope(it).rideRequestService().requestRide()
-        }
     }
 }
